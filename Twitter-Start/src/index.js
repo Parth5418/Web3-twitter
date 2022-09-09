e5e28e6836bc1656ed07101bc325fd059b8754bb
Parth <navadiyaparth682@gmail.com>
2022-09-09 09:03:12 +0530

    Initial commit

---
+++ Twitter-Start/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from "react-router-dom";
+import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from 'web3uikit';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MoralisProvider appId="SMhf0ByyrniPKyd0o2QoYQ3LqyJky9Z1q9N7399g" serverUrl="https://3eqb6skntnyu.usemoralis.com:2053/server">
+      <NotificationProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      </NotificationProvider>
+    </MoralisProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
